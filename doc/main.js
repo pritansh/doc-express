7be36e3cb2c0a30faf32cfd596c095d561692747
Pritansh Chaudhary <pritansh.chaudhary@gmail.com>
2017-06-06 01:35:11 +0530

    Post request along with body added

--- doc/main.js
+++ doc/main.js
@@ -1,7 +1,15 @@
 const express = require('express'),
+    bodyparser = require('body-parser'),
+    prismjs = require('prismjs'),
+    prettyjson = require('prettyjson'),
     app = express(),
     url = 'localhost',
     port = 9993
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({
+    extended: true
+}))
 
 app.set('views', `${__dirname}/views/`)
 app.set('view engine', 'ejs')
@@ -19,5 +27,9 @@ app.get('/json', (request, response) => response.send(JSON.stringify({
 app.get('/model/:name', (request, response) => response.send(JSON.stringify({
     routes: require('./doc.json')['models'].filter((model_name) => model_name['model'] == request.params.name)[0].routes
 })))
+
+app.post('/syntax', (request, response) => response.send({
+        html: `<code class=\"language-javascript\" style=\"width:70vw;\">${prismjs.highlight(JSON.stringify(JSON.parse(request.body.text), null, '\t'), prismjs.languages.javascript)}</code>`
+}))
 
 app.listen(port, () => console.log(`Documentation Server running on port ${port}`))
